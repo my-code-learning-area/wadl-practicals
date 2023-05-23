2bd1196a726316f2ae2889cf0b4f884e2b0b2c53
sumit kawale <sumitkawale2002@gmail.com>
2023-05-23 20:04:40 +0530

    saving

--- CC/05_tableOf_10/main.py
+++ CC/05_tableOf_10/main.py
@@ -1,14 +1,18 @@
 import webapp2
 
 class Main(webapp2.RequestHandler):
+
     def get(self):
+        number = 10
+        self.response.write("<a><button>Click On Meee</button></a>")
         self.response.write("<pre>")
 
         for i in range(1, 11):
-            self.response.write("10 x ")
+            self.response.write(number)
+            self.response.write(" x ")
             self.response.write(i)
             self.response.write(" = ")
-            self.response.write(10 * i)
+            self.response.write(number * i)
             self.response.write("<br>")
 
         self.response.write("</pre>")
